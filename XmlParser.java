0cfa76fe503d81e569b9d187fc575f840baa9ebe
Sara Bahrami <sbh@Saras-MacBook-Pro.local>
2018-01-26 09:51:38 -0800

    changing policy

--- XmlParser.java
+++ XmlParser.java
@@ -15,6 +15,7 @@
  License along with this program; if not, write to the Free
  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  GNU library General Public
+this is an example of policy
  */
 package relevantfile;
 
